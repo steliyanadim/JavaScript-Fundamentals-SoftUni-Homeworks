ba352eb3642d53b82eb0d62c90fdc0b43c348f85
steliyanadim <steliyanadim@gmail.com>
2023-01-02 13:39:09 +0200

    delete

--- Functions/Functions - Exer/smallestOfThreeNumbers.js
+++
@@ -1,5 +0,0 @@
-function smallestNumber(first, second, third) {
-    let smallestNum = Math.min(first, second, third)
-    console.log(smallestNum);
-}
-smallestNumber(2, 5, 3)
--- Functions/Functions - Lab/mathPower.js
+++
@@ -1,8 +0,0 @@
-function mathPower(number, power) {
-    let res = 1;
-    for(let i = 0; i < power; i++){
-        res *= number;
-    }
-    console.log(res);
-}
-mathPower(2,8)
